4efcf31521860527da58c624ceac6bd5c22024f9
1ba1 <vvvalenti01@yahoo.it>
2019-06-13 17:50:32 +0200

    add toggle and remove

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,6 @@
 let myLibrary = [];
-const book1 = new Book("First Book", "First Author", 1000, true); 
-const book2 = new Book("Second Book", "Second Author", 344, false); 
+const book1 = new Book("First Book", "First Author", 1000, true);
+const book2 = new Book("Second Book", "Second Author", 344, false);
 myLibrary.push(book1);
 myLibrary.push(book2);
 
@@ -11,63 +11,89 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+Book.prototype.toggle =  function() {
+  this.read ? this.read = false : this.read = true;
+}
+
 const modal = document.getElementById("myModal");
 const btn = document.getElementById("modalBtn");
 const span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function() {
   modal.style.display = "block";
-}
+};
 
 span.onclick = function() {
   modal.style.display = "none";
-}
+};
 
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
 
 function addBookToLibrary(event) {
   event.preventDefault();
-  const form = document.querySelector("form"); 
-  const book = new Book(form.title.value, form.author.value, form.pages.value, form.read.value);
+  const form = document.querySelector("form");
+  const book = new Book(
+    form.title.value,
+    form.author.value,
+    form.pages.value,
+    form.read.value
+  );
   myLibrary.push(book);
-  console.log(book);
-  console.log(myLibrary);
-  modal.style.display = "none"; 
+
+  modal.style.display = "none";
+
+  //clean form
+  const formFields = document.querySelectorAll("form input");
+  [...formFields].forEach(field => {
+    if (field.getAttribute("type") !== "radio") field.value = "";
+  });
+
   render();
 }
 
 const addBook = document.getElementById("addBook");
-//addBook.onclick = addBookToLibrary();
+addBook.addEventListener("click", addBookToLibrary, false);
 
-addBook.addEventListener("click", addBookToLibrary, false); 
+function removeBookFromLibrary(event) {
+  const cardIndex = event.target.parentNode.getAttribute("data-index");
+  myLibrary = myLibrary.filter((el, i) => i !== +cardIndex);
+  render();
+}
 
 function render() {
   const container = document.getElementsByClassName("container")[0];
-  while (container.hasChildNodes()) { container.removeChild(container.firstChild); } 
-  myLibrary.forEach((book) => {
-    const card = document.createElement("div"), 
-    header = document.createElement("header"), 
-    title = document.createElement("p"), 
-    author = document.createElement("p"), 
-    pages = document.createElement("p"), 
-    read = document.createElement("button"), 
-    remove = document.createElement("div");
+  while (container.hasChildNodes()) {
+    container.removeChild(container.firstChild);
+  }
+  myLibrary.forEach((book, index) => {
+    const card = document.createElement("div"),
+      header = document.createElement("header"),
+      title = document.createElement("p"),
+      author = document.createElement("p"),
+      pages = document.createElement("p"),
+      read = document.createElement("button"),
+      remove = document.createElement("div");
 
     card.setAttribute("class", "card");
     header.setAttribute("class", "header");
     read.setAttribute("class", "read");
     remove.setAttribute("class", "remove");
-    
+
     header.textContent = book.title;
-    title.textContent = "Title: \"" + book.title + "\"";
+    title.textContent = 'Title: "' + book.title + '"';
     author.textContent = "Author: " + book.author;
     pages.textContent = "No. of pages: " + book.pages;
-    read.textContent = (book.read ? "Read" : "Not read yet");
-    remove.innerHTML = "×"; 
+    read.textContent = book.read ? "Read" : "Not read yet";
+    read.onclick = () => {
+      book.toggle();
+      render();
+    }
+    remove.innerHTML = "×";
+    remove.addEventListener("click", removeBookFromLibrary, false);
 
     card.appendChild(header);
     card.appendChild(title);
@@ -75,7 +101,7 @@ function render() {
     card.appendChild(pages);
     card.appendChild(read);
     card.appendChild(remove);
-    
+    card.setAttribute("data-index", index);
     container.appendChild(card);
   });
 }
